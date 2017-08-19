4276ed562fd3470ad2c4376bdc2c4589208f7135
vitaliAI <vitali_mueller@yahoo.de>
2017-08-19 14:59:57 +0100

    Seting up Repository for ANN C++ Project

---
+++ ANN in C++/ANN in C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ANN in C++
+//
+//  Created by Vitali Mueller on 19/08/2017.
+//  Copyright © 2017 Vitali Mueller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
